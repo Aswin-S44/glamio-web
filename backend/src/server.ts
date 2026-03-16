94d416abe7c7e3e8b1fcb8765e0361201965f6f7
Aswin-S-T <aswinswebart15@gmail.com>
2026-03-16 21:32:35 +0530

    remove dist and firebase credentials

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+// new changes
 var corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
